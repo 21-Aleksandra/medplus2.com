f8244264db4cf2a57fec479730cf569c16e5824c
ravsk-alexandra <admitruka@edu.riga.lv>
2023-06-22 00:52:30 +0300

    Laravel breeze, models, migrations without testing

---
+++ app/Models/Doctor.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+use App\Models\Profession;
+use App\Models\Comment;
+use App\Models\Appointment;
+use App\Models\Language;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Doctor extends Model
+{
+    use HasFactory;
+    protected $fillable= ['name', 'gender', 'description', 'profession', 'subsidiary', 'phone']; 
+
+    public function profession()
+    {
+        return $this->belongsTo(Profession::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
+
+    public function languages()
+    {
+        return $this->belongsToMany(Language::class);
+    }
+
+}
---
+++ app/Models/Language.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+use App\Models\Doctor;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Language extends Model
+{
+    use HasFactory;
+
+    protected $fillable= ['name']; 
+
+    public function doctors()
+    {
+        return $this->belongsToMany(Doctor::class);
+    }
+
+}
